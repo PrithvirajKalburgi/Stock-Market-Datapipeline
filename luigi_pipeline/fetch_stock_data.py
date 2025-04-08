187529fb3d00dfd1f2157d32482ed292dd596087
Prithvi Work <kalburgi81@gmail.com>
2025-04-09 01:14:28 +0300

    Fetching stock data

--- luigi_pipeline/fetch_stock_data.py
+++ luigi_pipeline/fetch_stock_data.py
@@ -5,3 +5,28 @@ import pandas as pd
 from datetime import datetime
 from .config import API_SOURCE, SYMBOLS, FETCH_PERIOD, RAW_DATA_PATH
 
+class FetchStockData(luigi.Task):
+    """
+    Task to fetch stock data from yahoo finance (yfinance) API
+    """
+
+    symbol = luigi.Parameter()
+    date = luigi.DateParameter(default=datetime.now().date())
+
+    def output(self):
+        os.makedirs(RAW_DATA_PATH, exist_ok=True)
+        return luigi.LocalTarget(f"{RAW_DATA_PATH}/{self.symbol}_{self.date}.csv")
+    
+    def run(self):
+        stock = yf.Ticker(self.symbol)
+        data = stock.history(period=FETCH_PERIOD)
+
+        data.reset_index(inplace=True)
+
+        with self.output().open('w') as f:
+            data.to_csv(f, index=False)
+
+        print(f"Successfully fetched data for {self.symbol}")
+
+        
+
